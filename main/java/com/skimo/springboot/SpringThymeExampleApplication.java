bb509ed3c649bd12955615e185f41f6d97de266e
Aanya02 <aanyausa@gmail.com>
2020-06-23 23:12:11 -0700

    Add files via upload

---
+++ main/java/com/skimo/springboot/SpringThymeExampleApplication.java
@@ -0,0 +1,13 @@
+package com.skimo.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringThymeExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringThymeExampleApplication.class, args);
+	}
+
+}
